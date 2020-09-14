c342c7330d3d159cfd4b3d0e7dc6217bb1733e1b
Maria Fernanda Zermeño Reyna <mafernandazr@gmail.com>
2020-09-14 16:39:38 -0500

    se agrega pregunta 3 y se perfecciona el ocultar y mostrar

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 function muestra_oculta(id){
     if (document.getElementById){ //se obtiene el id
     var el = document.getElementById(id); //se define la variable "el" igual a nuestro div
-    el.style.display = (el.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
+    el.style.display = (el.style.display == 'block') ? 'none' : 'block'; //damos un atributo display:none que oculta el div
     }
     }
     window.onload = function(){/*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente*/
     muestra_oculta('.contenido');/* "contenido_a_mostrar" es el nombre que le dimos al DIV */
     }
+
+    function redireccionarPagina() {
+        window.open("https://ixhua.mx/#howworks")
+      }
+    
